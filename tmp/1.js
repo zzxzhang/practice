bd98a9e2e5b6b84915cd8bcefa0635ddebff9703
WIN-2TF0B8J7SH9\Administrator <zzx.zhang@gmail.com>
2021-12-09 23:06:59 +0800

    no message

--- tmp/1.js
+++ tmp/1.js
@@ -3,16 +3,16 @@ function test(i) {
     case "1":
       console.log("string");
       break;
-
     case 1:
       console.log("number");
       break;
-
     default:
       break;
   }
 
   
 }
+
+console.log(99)
 
 test(1);
